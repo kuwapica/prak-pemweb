2cb51a01b2c29c9bdab93bb69c036116b4e78b7e
kuwapica <annidaaiska@gmail.com>
2024-10-14 14:04:09 +0700

    update

--- kalkulator.php
+++ kalkulator.php
@@ -6,50 +6,22 @@
     <title>Document</title>
 </head>
 <body>
+    <h1>Aplikasi Kalkulator Sederhana</h1>
     <form action="kalkulator.php" method="post">
-        Angka 1: <input type="text" name="angka1"><br>
-        Angka 2: <input type="text" name="angka2"><br>
-        Operasi: 
-        <select name="operasi">
-            <option value="tambah">Tambah</option>
-            <option value="kurang">Kurang</option>
-            <option value="kali">Kali</option>
-            <option value="bagi">Bagi</option>
-        </select>
-        <br>
-        <input type="submit" name="hitung" value="Hitung">
+        <p>Input 1 : <input type="text" name="input1"></p>
+        <p>Input 2 : <input type="text" name="input2"> </p>
+        <p>
+            <input type="button" value="+">
+            <input type="button" value="-">
+            <input type="button" value="*">
+            <input type="button" value="/">
+        </p> 
+        <p>Hasil: </p>
     </form>
 
+
     <?php
-        if(isset($_POST['hitung'])){
-            $angka1 = $_POST['angka1'];
-            $angka2 = $_POST['angka2'];
-            $operasi = $_POST['operasi'];
-
-            switch ($operasi) {
-                case 'tambah':
-                    $hasil = $angka1 + $angka2;
-                    break;
-                case 'kurang':
-                    $hasil = $angka1 - $angka2;
-                    break;
-                case 'kali':
-                    $hasil = $angka1 * $angka2;
-                    break;
-                case 'bagi':
-                    $hasil = $angka1 / $angka2;
-                    break;            
-                default:
-                    break;
-            }
-            echo "Hasil: " . $hasil;
-        }
-
-        if(is_numeric($angka1) && is_numeric($angka2)){
-            // Kode operasi matematika
-        } else {
-            echo "<h2>Input harus berupa angka!</h2>";
-        }
+        echo "Hasil: " . $hasil;
     ?>
 </body>
 </html>
